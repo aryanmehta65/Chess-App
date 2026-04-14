e2f0a37086dcd7a73d78b28c1a57b093d93fea53
aryanmehta65 <aryan65mehta@gmail.com>
2026-04-14 18:12:58 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,7 +10,7 @@ st.header("Login")
 st.text_input("Username")
 st.text_input("Password",type="password")
 st.button("Login")
-col1,col2=st.columns([3,1])
+col1,col2=st.columns([1,0.3])
 with col1:
             st.write("Dont have Account?")
 with col2:
