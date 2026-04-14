0b52113caba1717b088f3487df6f41ac1bcf7181
aryanmehta65 <aryan65mehta@gmail.com>
2026-04-14 10:07:16 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ st.markdown("<h1 style ='color:lime; text-align:center;'>Checkmate-Chess</h1>",
             unsafe_allow_html=True)
 st.markdown("<h1 style ='color:yellow; text-align:center;'>Welcome!!</h1>",
             unsafe_allow_html=True)
-st.write("Login")
+st.header("Login")
 st.text_input("Username")
 st.text_input("Password",type="password")
 st.button("Login")
