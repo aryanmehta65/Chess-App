2228615b7d6e78c7abda59114a18f8eec9d9f0a4
aryanmehta65 <aryan65mehta@gmail.com>
2026-04-14 21:41:51 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,11 +13,12 @@ if st.button("Login"):
             st.write("logged in successfully!")
 st.write("Dont have Account?")
 if st.button("Sign in"):
-            st.text_input("Email")
-            st.text_input("Create Username")
-            st.text_input("Create Password",type="password")
-            if st.button("Submit"):
-                        st.write("Account Created")
-
+            with st.form("login_form"):
+            email=st.text_input("Email")
+            username=st.text_input("Create Username")
+            pasword=st.text_input("Create Password",type="password")
+            submit=st.form_submit_buttot("Sign in")
+            if submit:
+                        st.success(f"Welcome{username}")
 
 
